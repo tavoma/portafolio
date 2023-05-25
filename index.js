39d41f01af17c921287ba785bb72c2f2899a5c0b
Octavio Morales Alvarez <octaviomoralesalvarez@192.168.1.9>
2023-05-25 16:38:08 -0600

    Adicionó la funcionaliad de desencriptar

--- index.js
+++ index.js
@@ -26,3 +26,29 @@ function encriptar(){
     }
 }
 
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let mono = document.getElementById("mono");
+    
+   
+    let textoCifrado = texto
+        .replace(/enter/gi,"e")
+        .replace(/imes/gi,"i")
+        .replace(/ai/gi,"a")
+        .replace(/ober/gi,"o")
+        .replace(/ufat/gi,"u");
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        mono.src = "./img/desencriptado.jpg";
+    }else{
+        mono.src = "./img/Muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto a desencriptar";
+        alert("Se debe ingresar un texto");
+    }
+}
